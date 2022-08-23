88b13bbf7159b86cccc97cbbc9ec2aa4cae919c6
Hitesh Dutt <er.hiteshdutt@gmail.com>
2022-08-23 18:07:34 +0530

    minor changes to app and arm template

--- funcNodeJS/HttpTrigger/index.js
+++ funcNodeJS/HttpTrigger/index.js
@@ -3,50 +3,54 @@ module.exports = async function (context, req) {
     const method = req.method;
 
     const orchestrator = require('./orchestrator');
+    try {
+        if (action === 'deploy' && method === 'PUT') {
+            // here come call from resource provider to create resource.
+            // via orchestrator we will delegate work to another call of Azure function and quickly return response to resource provider.
+            const result = await orchestrator.callDeployFabricTools(context, req);
+            // body should be in 'properties' field for response to ARM
+            setResponse(result.status, { properties: result });
 
-    if (action === 'deploy' && method === 'PUT') {
-        // here come call from resource provider to create resource.
-        // via orchestrator we will delegate work to another call of Azure function and quickly return response to resource provider.
-        const result = await orchestrator.callDeployFabricTools(context, req);
-        // body should be in 'properties' field for response to ARM
-        setResponse(result.status, { properties: result });
+            return;
+        } else if (action === 'deploy' && method === 'GET') {
+            // here come call from resource provider to poll resource creation status.
+            const result = await orchestrator.getDeployFabricToolsStatus(context, req);
+            // body should be in 'properties' field for response to ARM
+            setResponse(result.status, { properties: result });
 
-        return;
-    } else if (action === 'deploy' && method === 'GET') {
-        // here come call from resource provider to poll resource creation status.
-        const result = await orchestrator.getDeployFabricToolsStatus(context, req);
-        // body should be in 'properties' field for response to ARM
-        setResponse(result.status, { properties: result });
+            return;
+        } else if (action === 'deployfabrictools' && method === 'PUT') {
+            // here come chaining call from orchestrator, this call will do main work.
+            context.log('Deploy fabric tools requested.');
+            const deployFabricTools = require('./fabric-tools-provisioner');
 
-        return;
-    } else if (action === 'deployfabrictools' && method === 'PUT') {
-        // here come chaining call from orchestrator, this call will do main work.
-        context.log('Deploy fabric tools requested.');
-        const deployFabricTools = require('./fabric-tools-provisioner');
+            // mark that remote orchestrator's call reached this function.
+            await orchestrator.setDeployFabricToolsStatus(context, req, {
+                status: 200,
+                provisioningState: 'Accepted'
+            });
 
-        // mark that remote orchestrator's call reached this function.
-        await orchestrator.setDeployFabricToolsStatus(context, req, {
-            status: 200,
-            provisioningState: 'Accepted'
-        });
+            const result = await deployFabricTools(req.body.properties, context.log);
+            context.log(`Deploy fabric tools finished.`);
 
-        const result = await deployFabricTools(req.body.properties, context.log);
-        context.log(`Deploy fabric tools finished.`);
+            await orchestrator.setDeployFabricToolsStatus(context, req, result);
+            setResponse(result.status, result);
 
-        await orchestrator.setDeployFabricToolsStatus(context, req, result);
-        setResponse(result.status, result);
-
-        return;
-    } else {
-        setResponse(409, {
-            error: {
-                message: `Unknown action ${action} and method ${method}`
-            },
-            provisioningState: 'Failed',
-            properties: {
-                provisioningState: 'Failed'
-            }
-        });
+            return;
+        } else {
+            setResponse(409, {
+                error: {
+                    message: `Unknown action ${action} and method ${method}`
+                },
+                provisioningState: 'Failed',
+                properties: {
+                    provisioningState: 'Failed'
+                }
+            });
+        }
+    }
+    catch (ex) {
+        throw `${action} ${method} ${JSON.stringify(req.params)} ${JSON.stringify(ex)}`;
     }
 
     function setResponse(status, body) {
